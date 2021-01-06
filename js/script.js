46edc5ac3ee379ceb9dfd07d0fced6f9bc4cc811
ljilj <ljilja.antonic91@gmail.com>
2021-01-06 18:06:25 +0100

    Modified Header, Footer and Created Hero section

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // Toggle menu
 const navToggle = document.querySelector('.js-menu-toggle');
-const links = document.querySelector('.links__container');
+const links = document.querySelector('.c-nav-links__container');
 
 navToggle.addEventListener('click', function() {
     links.classList.toggle("active-menu");
