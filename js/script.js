9542d01ab0cd51dccadd339c35bef72dd9295553
ljilj <ljilja.antonic91@gmail.com>
2021-01-11 09:19:14 +0100

    Created Companies section, Smart Jackpot section, Designed section, Our app section, Ultimate section, Cards section, Faq section and Download section

--- js/script.js
+++ js/script.js
@@ -5,3 +5,15 @@ const links = document.querySelector('.c-nav-links__container');
 navToggle.addEventListener('click', function() {
     links.classList.toggle("active-menu");
 });
+
+
+// Toggle-switch
+var btn = document.getElementById('js-btn');
+
+function monthlyClick() {
+	btn.style.left = '2px'; 
+}
+
+function annualyClick() {
+	btn.style.left = '110px';
+}
